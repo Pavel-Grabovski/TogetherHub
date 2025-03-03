f7dfbd35e15f3600f3dc7c53f35e957bd4174391
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-04 04:31:04 +0700

    add class Topic

---
+++ Domain/Model/Topic.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+
+namespace Domain.Model;
+
+public class Topic : Entity<TopicId>
+{
+    public required string Title { get; set; }
+    public DateTime? EventStart { get; set; }
+    public required string Summary { get; set; }
+    public required string TopicType { get; set; }
+    public required Location Location { get; set; }
+
+
+    public static Topic Create(
+        TopicId id,
+        string title,
+        DateTime eventStart,
+        string summary,
+        string topicType,
+        Location location)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
+        ArgumentException.ThrowIfNullOrWhiteSpace(summary);
+        ArgumentException.ThrowIfNullOrWhiteSpace(topicType);
+
+        Topic topic = new Topic
+        {
+            Id = id,
+            Title = title,
+            EventStart = eventStart,
+            Summary = summary,
+            TopicType = topicType,
+            Location = location
+        };
+
+        return topic;
+    }
+}
