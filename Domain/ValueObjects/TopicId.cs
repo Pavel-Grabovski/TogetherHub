4b3e8c1f7490ee3b3a363847dac7938766bb41a0
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-04 04:06:51 +0700

    create add TopicId

---
+++ Domain/ValueObjects/TopicId.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects;
+
+public record TopicId
+{
+    public Guid Value { get; }
+
+    private TopicId(Guid value)
+    {
+        this.Value = value;
+    }
+
+    public static TopicId Of(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new DomainException("TopicId cannot be empty.");
+
+        return new TopicId(value);
+    }    
+}
