66bf68ce8d690c457e67dd31d48189f843733506
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-06 02:34:40 +0700

    create IApplicationDbContext

---
+++ Application/Data/DataBaseContext/IApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data.DataBaseContext;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Topic> Topics { get; }
+}
