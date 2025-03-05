43c523c610b744a317c9b860cb02eb30c46ebe30
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-06 02:49:29 +0700

    Настройка базы данных

---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DataBaseContext;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Topic> Topics
+    {
+        get => Set<Topic>();
+    }
+
+    public ApplicationDbContext(DbContextOptions options) 
+        : base(options)
+    {
+
+    }
+}
