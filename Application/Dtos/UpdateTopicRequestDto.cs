05a98b78670be593905baf29d3db66840402f138
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-12 06:50:31 +0700

    fix UpdateTopicRequestDto all properties is nullable  type

--- Application/Dtos/UpdateTopicRequestDto.cs
+++ Application/Dtos/UpdateTopicRequestDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace Application.Dtos;
 
 public record UpdateTopicRequestDto(
-    string Title,
-    string Summary,
-    string TopicType,
-    LocationRequestDto Location,
+    string? Title,
+    string? Summary,
+    string? TopicType,
+    LocationRequestDto? Location,
     DateTime? EventStart
 );
