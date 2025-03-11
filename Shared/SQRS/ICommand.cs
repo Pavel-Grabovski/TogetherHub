b2f420503c3c4cc2e60c762ebffb2ceaa1fe9807
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-11 23:40:55 +0700

    create: ICommand, ICommandHandler, IQuery, IQueryHandler

---
+++ Shared/SQRS/ICommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.SQRS;
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+
+}
+
+public interface ICommand : IRequest<Unit>
+{
+
+}
---
+++ Shared/SQRS/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.SQRS;
+
+public interface IQuery<out TResponse>
+    : IRequest<TResponse>
+{
+
+}
