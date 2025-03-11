4a3c120a779fdb3bd650bd8c1116ca8455c3e311
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-11 21:56:35 +0700

    create: migration db soft delete

---
+++ Infrastructure/Migrations/20250311142223_Add soft delete field.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addsoftdeletefield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "Topics",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Topics",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "Topics");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Topics");
+        }
+    }
+}
