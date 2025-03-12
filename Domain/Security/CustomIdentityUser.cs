1f6955efc28c9dde299998a8f183d1dcd761e0b1
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-12 22:57:14 +0700

    Create model CustomIdentityUser

---
+++ Domain/Security/CustomIdentityUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Security;
+
+public class CustomIdentityUser : IdentityUser
+{
+    public string? FullName { get; set; }
+    public string? About { get; set; }
+
+}
