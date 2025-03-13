937ba9288a745e670862cdd4b18300ffab2d8c76
Pavel Grabovskiy <Araa9977@mail.ru>
2025-03-13 23:48:34 +0700

    create  AuthController and Auth login

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Security;
+using Domain.Security.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(UserManager<CustomIdentityUser> userManager) 
+    : ControllerBase
+{
+    [HttpPost]
+    [Route("login")]
+    public async Task<IResult> Login(LoginResponseDto dto)
+    {
+        CustomIdentityUser? user = await userManager.FindByEmailAsync(dto.Email);
+
+        if (user == null)
+            return Results.Unauthorized();
+
+        bool result = await userManager.CheckPasswordAsync(user, dto.Password);
+
+        if (result)
+        {
+            IdentityUserResponseDto response = new IdentityUserResponseDto(
+                user.UserName,
+                user.Email,
+                "jwt");
+
+            return Results.Ok(new {result = response});
+        }
+
+        return Results.Unauthorized();
+    }
+}
