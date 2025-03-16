971dd1e2ac87be917d3b48b845e8d1fe1ff2b636
Home <araa9977@mail.ru>
2025-03-17 03:37:22 +0700

    Добавил получение userId из UserAccessor

--- Application/Security/Services/IUserAccessor.cs
+++ Application/Security/Services/IUserAccessor.cs
@@ -3,4 +3,6 @@
 public interface IUserAccessor
 {
     public string GetUsername();
+
+    public string GetUserId();
 }
--- Application/Security/Services/UserAccessor.cs
+++ Application/Security/Services/UserAccessor.cs
@@ -6,6 +6,13 @@ namespace Application.Security.Services;
 public class UserAccessor(IHttpContextAccessor httpContextAccessor) 
     : IUserAccessor
 {
+    public string GetUserId()
+    {
+        return httpContextAccessor
+            .HttpContext!
+            .User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+    }
+
     public string GetUsername()
     {
         return httpContextAccessor
