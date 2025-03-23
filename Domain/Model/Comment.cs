56a0a25ac1e06c6f491824506e4ad83b6dc3ac1e
Home <araa9977@mail.ru>
2025-03-23 13:41:28 +0700

    create entity Comment and CommentId

---
+++ Domain/Model/Comment.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Model;
+
+public class Comment : Entity<CommentId>
+{
+    public required User Author { get; set; }
+
+    public required Topic CurrentTopic { get; set; }
+
+    public required string Text { get; set; }
+
+    public static Comment Create(
+        CommentId commentId,
+        User author,
+        Topic topic,
+        string text)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(text);
+
+        return new Comment
+        {
+            Id = commentId,
+            Author = author,
+            CurrentTopic = topic, 
+            Text = text
+        };
+    }
+}
---
+++ Domain/ValueObjects/CommentId.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.ValueObjects;
+
+public record CommentId
+{
+    public Guid Value { get; }
+
+    private CommentId(Guid value)
+    {
+        this.Value = value;
+    }
+
+    public static CommentId Of(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new DomainException("CommentId cannot be empty.");
+
+        return new CommentId(value);
+    }
+}
